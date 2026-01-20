7753802a18a9f8a64f427468f2dffadf0120dd07
Amitav <amitavrchy01@gmail.com>
2026-01-20 16:15:58 +0600

    a23

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5056;
 const MONGODB_URI = process.env.DB_URL || "mongodb+srv://fahadpervej:adminFahadPervej@cluster0.9o8rsbr.mongodb.net/?appName=Cluster0";
-
+console.log("aaaa");
 const httpServer = http.createServer(app);
 
 // ✅ Connect MongoDB
