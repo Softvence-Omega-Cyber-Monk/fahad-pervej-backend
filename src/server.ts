a31ed39cd9ba0073a77312beed5003f0ce7e4e81
Amitav <amitavrchy01@gmail.com>
2026-01-20 15:55:58 +0600

    added

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { ChatRoute } from "./app/modules/chat/chat.route";
 dotenv.config();
 
 const PORT = process.env.PORT || 5056;
-const MONGODB_URI = process.env.DB_URL
+const MONGODB_URI = process.env.DB_URL || "mongodb+srv://fahadpervej:adminFahadPervej@cluster0.9o8rsbr.mongodb.net/?appName=Cluster0";
 
 console.log(MONGODB_URI);
 
